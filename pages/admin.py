e774d23250909efaec0500f425e33f40f96eb21e
vida-hashemzadeh <v.hashemzadeh@gmail.com>
2021-03-08 13:07:24 +0330

    update2_ team model

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Team
+from django.utils.html import format_html
 
+
+class TeamAdmin(admin.ModelAdmin):
+    def Imageteam (self,object):
+        return format_html('<img src="{}" width="50" style="border-radius: 50%;" />'.format(object.photo.url))
+
+    Imageteam.short_description = "Image"
+
+    list_display = ('id','Imageteam','first_name','last_name','designation')
+    list_display_links = ('id','Imageteam','first_name',)
+    search_fields = ('first_name' , 'last_name')
+    list_filter = ('first_name','last_name','designation')
 # Register your models here.
+admin.site.register (Team,TeamAdmin)
